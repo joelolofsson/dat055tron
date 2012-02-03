2ecedb8a45db88259ddffa81fde57b593288b90f
zottyx@gmail.com <zottyx@gmail.com@2dc362f9-eeb6-9768-ecee-41cbf4441113>
2012-02-03 08:47:51 +0000

    Skapar klassen player och implementerat instansvariabler, konstruktor och metoder.

---
+++ trunk/tron/src/Player.java
@@ -0,0 +1,60 @@
+import java.awt.Color;
+
+public class Player {
+
+	private Color color;
+	private int course;
+	private int posX;
+	private int posY;
+	private String name;
+	private int score = 0;
+	private boolean alive = true;
+	
+	public Player(Color color, String name, int course, int posX, int posY){
+		this.color = color;
+		this.name = name;
+		this.course = course;
+		this.posX = posX;
+		this.posY = posY;
+	}
+	
+	public Color getColor() {
+		return color;
+	}
+	
+	public int getCourse() {
+		return course;
+	}
+	
+	public int getPosX() {
+		return posX;
+	}
+	
+	public int getPosY() {
+		return posY;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	public void setCourse(int course) {
+		this.course = course;
+	}
+	
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+	
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+}
